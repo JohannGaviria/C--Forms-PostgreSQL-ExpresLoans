c05539226ec24570cfeb3f18a2147c242315c52d
JohannGaviria <johannmgr@gmail.com>
2024-05-20 09:45:58 -0500

    Crear logica para la pantalla de saldo
    En este commit se crea la logica para agregar saldo y mostrar el historial:
    
    - Crea logica para agregar saldo
    - Crea logica para agregar historial de saldo
    - Crea logica para mostrar el historial de saldo

--- frmBalance.cs
+++ frmBalance.cs
@@ -78,13 +78,65 @@ namespace expressLoan
                 return;
             }
 
-            MessageBox.Show("Saldo Agregado");
+            Conexion.Conexion conexion = new Conexion.Conexion();
+            NpgsqlConnection conn = conexion.connection();
 
-            if (currentModal != null)
+            if (conn.State == System.Data.ConnectionState.Open)
             {
-                this.Controls.Remove(currentModal);
-                currentModal = null;
+                try
+                {
+                    // Transacción para asegurar la integridad de las inserciones
+                    using (var transaction = conn.BeginTransaction())
+                    {
+                        // Inserción en la tabla 'saldos'
+                        string insertSaldosQuery = "INSERT INTO saldos (usuario_id, saldo) VALUES (@usuario_id, @saldo)";
+                       
+                        using (NpgsqlCommand cmd = new NpgsqlCommand(insertSaldosQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@usuario_id", UserSession.UserId);
+                            cmd.Parameters.AddWithValue("@saldo", txtSaldo.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        // Inserción en la tabla 'historial_saldo'
+                        string insertHistorialQuery = "INSERT INTO historial_saldo (id_usuario, fecha, tipo_movimiento, monto) VALUES (@id_usuario, CURRENT_TIMESTAMP, @tipo_movimiento, @monto)";
+                        using (NpgsqlCommand cmd = new NpgsqlCommand(insertHistorialQuery, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@id_usuario", UserSession.UserId);
+                            cmd.Parameters.AddWithValue("@tipo_movimiento", "Depósito");
+                            cmd.Parameters.AddWithValue("@monto", txtSaldo.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        // Confirmar la transacción
+                        transaction.Commit();
+                    }
+
+                    MessageBox.Show("Saldo Agregado");
+
+                    CargarSaldo();
+                    CargarHistorialSaldo();
+
+                    if (currentModal != null)
+                    {
+                        this.Controls.Remove(currentModal);
+                        currentModal = null;
+                    }
+                }
+                catch (NpgsqlException ex)
+                {
+                    MessageBox.Show("Error al ingresar el saldo: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
             }
+            else
+            {
+                MessageBox.Show("No se pudo establecer la conexión a la base de datos.");
+            }
+
         }
 
         private void OnAceptarRetirar(object sender, EventArgs e)
@@ -110,8 +162,8 @@ namespace expressLoan
 
         private bool ValidarSaldo(string saldo)
         {
-            // Validar que el número de celular tenga 10 dígitos y sea numérico
-            return Regex.IsMatch(saldo, @"^\d{4}$");
+            // Validar que el saldo sea numérico, opcionalmente con decimales
+            return Regex.IsMatch(saldo, @"^\d+(\.\d{1,2})?$");
         }
 
         // Método para mostrar mensajes de error
@@ -122,6 +174,12 @@ namespace expressLoan
 
         private void frmBalance_Load(object sender, EventArgs e)
         {
+            CargarSaldo();
+            CargarHistorialSaldo();
+        }
+
+        private void CargarSaldo()
+        {
             Conexion.Conexion conexion = new Conexion.Conexion();
             NpgsqlConnection conn = conexion.connection();
 
@@ -129,7 +187,7 @@ namespace expressLoan
             {
                 try
                 {
-                    string query = "SELECT saldo FROM saldos WHERE usuario_id = @usuario_id";
+                    string query = "SELECT SUM(CAST(saldo AS numeric)) FROM saldos WHERE usuario_id = @usuario_id";
                     using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
                     {
                         cmd.Parameters.AddWithValue("usuario_id", UserSession.UserId);
@@ -147,7 +205,7 @@ namespace expressLoan
                 }
                 catch (NpgsqlException ex)
                 {
-                    MessageBox.Show("Error al iniciar sesión: " + ex.Message);
+                    MessageBox.Show("Error al cargar el saldo: " + ex.Message);
                 }
                 finally
                 {
@@ -159,5 +217,57 @@ namespace expressLoan
                 MessageBox.Show("No se pudo establecer la conexión a la base de datos.");
             }
         }
+
+        private void CargarHistorialSaldo()
+        {
+            Conexion.Conexion conexion = new Conexion.Conexion();
+            NpgsqlConnection conn = conexion.connection();
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    string query = "SELECT fecha, tipo_movimiento, monto FROM historial_saldo WHERE id_usuario = @id_usuario";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id_usuario", UserSession.UserId);
+
+                        NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                        // Limpiar filas existentes y definir columnas si es necesario
+                        dgvHistorial.Rows.Clear();
+                        if (dgvHistorial.Columns.Count == 0)
+                        {
+                            dgvHistorial.Columns.Add("fecha", "Fecha");
+                            dgvHistorial.Columns.Add("tipo_movimiento", "Tipo de Movimiento");
+                            dgvHistorial.Columns.Add("monto", "Monto");
+                        }
+
+                        while (reader.Read())
+                        {
+                            // Agregar fila al DataGridView
+                            dgvHistorial.Rows.Add(reader["fecha"], reader["tipo_movimiento"], reader["monto"]);
+                        }
+                        dgvHistorial.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+                    }
+                }
+                catch (NpgsqlException ex)
+                {
+                    MessageBox.Show("Error al cargar el historial de saldo: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se pudo establecer la conexión a la base de datos.");
+            }
+        }
+
+
     }
 }
